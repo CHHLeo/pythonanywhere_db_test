8ad3b64a113d35579f3107e7768c7fe800fcc84b
unknown <MTK09942@PC14110378.mediatek.inc>
2015-12-14 19:25:21 +0800

    python any where db test

---
+++ flask_pycharm_practice.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+user_name = 'ch2leo'
+password = '21070527'
+database_host_address = 'ch2leo.mysql.pythonanywhere-services.com'
+database_name = 'ch2leo$test'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://' + user_name + ':' + password + '@' + database_host_address + '/' + database_name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, unique=False)
+    is_completed = db.Column(db.Boolean)
+
+    def __init__(self, title):
+        self.title = title
+        self.is_completed = False
+
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    db.session.add(Todo(title=request.form['title']))
+    db.session.commit()
+    return redirect('/')
+
+
+app.debug = True
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
